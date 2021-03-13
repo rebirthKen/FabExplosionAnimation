c0c6d02b768435f2a19207d9e91ccda03a30bc00
chao shi <cshi1017@gmail.com>
2021-03-13 10:35:14 -0600

    Animation Demo

--- app/src/main/java/com/example/fabexplosionanimation/MainActivity.kt
+++ app/src/main/java/com/example/fabexplosionanimation/MainActivity.kt
@@ -1,38 +1,39 @@
 package com.example.fabexplosionanimation
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
+import android.view.animation.AccelerateDecelerateInterpolator
+import android.view.animation.AnimationUtils
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
+import com.example.fabexplosionanimation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setSupportActionBar(findViewById(R.id.toolbar))
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        val animation = AnimationUtils.loadAnimation(this, R.anim.circle_explosion_animation).apply {
+            duration = 1000
+            interpolator = AccelerateDecelerateInterpolator()
         }
-    }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+        binding.fab.setOnClickListener {
+            binding.fab.isVisible = false
+            binding.circleView.isVisible = true
+            binding.circleView.startAnimation(animation) {
+                //todo you can display the view or whatever you want when the animation is done
+                binding.root.setBackgroundColor(ContextCompat.getColor(this, R.color.purple_200))
+                binding.circleView.isVisible = false
+                binding.textView.isVisible = true
+            }
         }
+
     }
+
+
 }
